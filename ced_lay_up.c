9c99b54b206ef850f883e28bf7e631902b9282e1
lennuy_f <lennuy_f@epitech.eu>
2014-04-21 18:37:06 +0200

    Bite ?

---
+++ ced_lay_up.c
@@ -0,0 +1,14 @@
+/*
+** ced_lay_up.c for ced_lay_up in /home/lennuy_f/rendu/42sh
+** 
+** Made by lennuy_f
+** Login   <lennuy_f@epitech.net>
+** 
+** Started on  Mon Apr 21 18:36:14 2014 lennuy_f
+** Last update Mon Apr 21 18:36:44 2014 lennuy_f
+*/
+
+void	lay_up(char **cedric)
+{
+  return (NULL);
+}
