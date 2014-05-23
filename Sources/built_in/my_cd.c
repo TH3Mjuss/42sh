82bfde20cc70b9bacf351fb38a7e0e6909a14083
lennuy_f <lennuy_f@epitech.eu>
2014-05-23 15:46:45 +0200

    cd is done

--- Sources/built_in/my_cd.c
+++ Sources/built_in/my_cd.c
@@ -5,7 +5,7 @@
 ** Login   <beauge_z@epitech.net>
 **
 ** Started on  Mon Apr  7 20:10:41 2014 Zackary Beaugelin
-** Last update Sun May 18 19:00:05 2014 lennuy_f
+** Last update Wed May 21 17:54:32 2014 lennuy_f
 */
 
 #include "my_sh.h"
@@ -33,6 +33,7 @@ char	*my_find(char **env, int k, char *to_find)
 char	**my_cd(char **param, char **env)
 {
   char  *dir;
+  char	buff[4096];
 
   dir = NULL;
   if (param[1] == NULL || my_strcmp(param[1], "~") == 0
@@ -51,25 +52,13 @@ char	**my_cd(char **param, char **env)
 	  return (env);
 	}
       chdir(dir);
-      my_putstr(my_find(env, 0, "OLDPWD"), 1);
-      write(1, "\n", 1);
       env = my_setenv(env, "OLDPWD", my_find(env, 0, "PWD"));
       env = my_setenv(env, "PWD", dir);
     }
   else if (chdir(param[1]) != -1)
     {
-      if (my_strncmp(param[1], "..", 2) == 0)
-	{
-	  env = my_setenv(env, "OLDPWD", my_find(env, 0, "PWD"));
-	  env = my_setenv(env, "PWD",
-			  my_setop(my_find(env, 0, "PWD"), (const char *)param[1]));
-	}
-      else
-	{
-	  env = my_setenv(env, "OLDPWD", my_find(env, 0, "PWD"));
-	  env = my_setenv(env, "PWD", my_strcat(my_find(env, 0, "PWD"),
-						my_strcat("/", param[1])));
-	}
+      env = my_setenv(env, "OLDPWD", my_find(env, 0, "PWD"));
+      env = my_setenv(env, "PWD", getcwd(buff, 4096));
     }
   else
     {
